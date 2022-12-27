a248e3e8dd3cb8edc534697429373fd3e21dc369
WinterSunset95 <wintersunset95@gmail.com>
2022-12-27 20:43:29 +0530

    added react

--- js/index.js
+++ js/index.js
@@ -46,43 +46,69 @@ const next = () => {
 
 const events = [
 	{
-	  date: 'Jan 1, 2021',
-	  name: 'New Year's Day',
-	  image: 'file:///C:/Users/PC/Desktop/New%20folder%20(2)/htdocs/ccpur_connect/images/new-years.webp',
+		date: 'Jan 1, 2021',
+		name: 'New Year\'s Day',
+		image: '../images/new-years.webp',
 	},
 	{
-	  date: 'Feb 14, 2021',
-	  name: 'Valentine's Day',
-	  image: 'C:\Users\PC\Desktop\New folder (2)\htdocs\ccpur_connect\images\Valentine-s-Day.png',
+		date: 'Feb 14, 2021',
+		name: 'Valentine\'s Day',
+		image: '../images/Valentine-s-Day.png',
 	},
 	{
-	  date: 'Mar 17, 2021',
-	  name: 'St. Patrick's Day',
-	  image: 'https://example.com/images/st-patricks-day.jpg',
+		date: 'Mar 17, 2021',
+		name: 'St. Patrick\'s Day',
+		image: 'https://unsplash.it/600/400', // You can use this link when you want placeholder images
 	},
-  ];
-  
-  const eventsContainer = document.getElementById('events');
-  
-  for (const event of events) {
-	const eventElement = document.createElement('div');
-	eventElement.classList.add('event');
-  
-	const eventDate = document.createElement('div');
-	eventDate.classList.add('event-date');
-	eventDate.textContent = event.date;
-  
-	const eventName = document.createElement('div');
-	eventName.classList.add('event-name');
-	eventName.textContent = event.name;
-  
-	const eventImage = document.createElement('img');
-	eventImage.classList.add('event-image');
-	eventImage.src = event.image;
-  
-	eventElement.appendChild(eventDate);
-	eventElement.appendChild(eventImage);
-	eventElement.appendChild(eventName);
-  
-	eventsContainer.appendChild(eventElement);
-  }
+];
+
+// We are using a library called 'ReactJS' here
+// Declaring a react component
+function Events() {
+	return (
+		<div clasName="events-container">
+			{
+				// google search 'javascript array.map()'
+				events.map(item => {
+					// For every item in the 'events' array -
+					return (
+						<div className="event">
+							<img className="event-image" src={item.image} alt="" />
+							<div>
+								<div>{item.date}</div>
+								<div>{item.name}</div>
+							</div>
+						</div>
+					)
+				})
+			}
+		</div>
+	)
+}
+
+const eventsContainer = document.getElementById('events');
+// Render 'Events' component to the specified element
+ReactDOM.render(<Events />, eventsContainer)
+
+//for (const event of events) {
+//	const eventElement = document.createElement('div');
+//	eventElement.classList.add('event');
+//
+//	const eventDate = document.createElement('div');
+//	eventDate.classList.add('event-date');
+//	eventDate.textContent = event.date;
+//
+//	const eventName = document.createElement('div');
+//	eventName.classList.add('event-name');
+//	eventName.textContent = event.name;
+//
+//	const eventImage = document.createElement('img');
+//	eventImage.classList.add('event-image');
+//	eventImage.src = event.image;
+//
+//	eventElement.appendChild(eventDate);
+//	eventElement.appendChild(eventImage);
+//	eventElement.appendChild(eventName);
+//
+//	eventsContainer.appendChild(eventElement);
+//}
